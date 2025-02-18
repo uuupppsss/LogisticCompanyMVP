69f844fd6a25605f535bd776b370e9d3c097e98a
uuupppsss <ulya.ch.06@mail.ru>
2025-02-18 16:34:22 +1000

    Добавьте файлы проекта.

---
+++ LogisticCompanyMVP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LogisticCompanyMVP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ LogisticCompanyMVP/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticCompanyMVP.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ArriveDate { get; set; }
+    }
+}
